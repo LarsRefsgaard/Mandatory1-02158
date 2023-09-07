d094a37ff8b57df9e44c2b1c80efcace938b842a
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2023-09-07 23:36:46 +0200

    Tweak search patterns to make time longer

--- LargeFile.py
+++ LargeFile.py
@@ -2,11 +2,11 @@ import random
 import string
 
 # Define the search pattern
-search_pattern = "0"
+search_pattern = "ABCDEFDGHIJKL"
 
 # Generate random text with minimal occurrences of the search pattern
 def generate_random_text(file_size, pattern, max_pattern_occurrences):
-    text = '\n'.join(random.choice(string.ascii_letters) for _ in range(file_size))
+    text = ''.join(random.choice(string.ascii_letters) for _ in range(file_size))
     pattern_occurrences = 0
     while pattern_occurrences < max_pattern_occurrences:
         position = random.randint(0, len(text) - len(pattern))
@@ -15,8 +15,8 @@ def generate_random_text(file_size, pattern, max_pattern_occurrences):
     return text
 
 # Define the file size and maximum pattern occurrences
-file_size = 1000000  # Adjust this to your desired file size
-max_pattern_occurrences = 5  # Adjust this to control the number of pattern occurrences
+file_size = 10000000 # Adjust this to your desired file size
+max_pattern_occurrences = 256  # Adjust this to control the number of pattern occurrences
 
 # Generate random text with the search pattern
 random_text = generate_random_text(file_size, search_pattern, max_pattern_occurrences)
